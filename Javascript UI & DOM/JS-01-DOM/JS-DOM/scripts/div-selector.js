8f90977115bdb74f693af1568cef5d1bdaae62cc
PolyaNM <polya.nm@gmail.com>
2014-06-02 17:48:00 +0300

    DOM

---
+++ Javascript UI & DOM/JS-01-DOM/JS-DOM/scripts/div-selector.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict'
+    function getDivsViaQuerySelectorAll() {
+        return document.querySelectorAll('div>div');
+    }
+
+    function getDivsViaGetElementsByTagName() {
+        var allDivs = document.getElementsByTagName('div'),
+            liveDivList = [];
+
+        for (var i = 0, len = allDivs.length; i < len; i++) {
+            if (allDivs[i].parentElement instanceof HTMLDivElement) {
+                liveDivList.push(allDivs[i]);
+            }
+        }
+
+        return liveDivList;
+    }
+
+    console.log(getDivsViaQuerySelectorAll());
+    console.log(getDivsViaGetElementsByTagName());
+})();
+
