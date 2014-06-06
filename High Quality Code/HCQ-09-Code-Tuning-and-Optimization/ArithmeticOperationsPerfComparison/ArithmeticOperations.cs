427b7ce1a2f557d055a30a24794c1fea9bd51982
PolyaNM <polya.nm@gmail.com>
2014-06-06 13:07:03 +0300

    commit

---
+++ High Quality Code/HCQ-09-Code-Tuning-and-Optimization/ArithmeticOperationsPerfComparison/ArithmeticOperations.cs
@@ -0,0 +1,355 @@
+﻿// --------------------- CONCLUSION ---------------------
+// Operations with decimal are the slowest
+// Long - division is slowest, addition - fastest
+// Float, Double - division and multiplication are slower
+
+namespace ArithmeticOperationsPerfComparison
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class ArithmeticOperations
+    {
+        public const int IterationsCount = 1000000;
+        public const int PaddingCount = 30;
+
+        public static void DisplayPerformanceTime(Action action, string message)
+        {
+            Console.Write(message);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed + System.Environment.NewLine);
+        }
+
+        public static void Add()
+        {
+            string intMessage = "Addition of int: ".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    int sum = 0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        sum += 50;
+                    }
+                },
+                intMessage);
+
+            string longMessage = "Addition of long: ".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    long sum = 0L;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        sum += 50L;
+                    }
+                },
+                longMessage);
+
+            string floatMessage = "Addition of float: ".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float sum = 0f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        sum += 50f;
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Addition of double: ".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double sum = 0.0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        sum += 50.0;
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Addition of decimal: ".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal sum = 0m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        sum += 50m;
+                    }
+                },
+                decimalMessage);
+        }
+
+        public static void Subtract()
+        {
+            string intMessage = "Subtraction of int:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    int result = 1000000000;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result -= 50;
+                    }
+                },
+                intMessage);
+
+            string longMessage = "Subtraction of long:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    long result = 1000000000L;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result -= 50L;
+                    }
+                },
+                longMessage);
+
+            string floatMessage = "Subtraction of float:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float result = 1000000000f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result -= 50f;
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Subtraction of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double result = 1000000000.0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result -= 50.0;
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Subtraction of decimal:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal result = 1000000000m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result -= 50m;
+                    }
+                },
+                decimalMessage);
+        }
+
+        public static void Increment()
+        {
+            string intMessage = "Increment of int:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    int value = 1;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        value++;
+                    }
+                },
+                intMessage);
+
+            string longMessage = "Increment of long:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    long value = 1L;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        value++;
+                    }
+                },
+                longMessage);
+
+            string floatMessage = "Increment of float:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float value = 1f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        value++;
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Increment of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double value = 1.0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        value++;
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Increment of decimal:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal value = 1m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        value++;
+                    }
+                },
+                decimalMessage);
+        }
+
+        public static void Multiply()
+        {
+            string intMessage = "Multiplication of int:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    int product = 1;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        product *= 1;
+                    }
+                },
+                intMessage);
+
+            string longMessage = "Multiplication of long:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    long product = 1L;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        product *= 1L;
+                    }
+                },
+                longMessage);
+
+            string floatMessage = "Multiplication of float:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float product = 1f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        product *= 1f;
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Multiplication of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double product = 1.0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        product *= 1.0;
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Multiplication of decimal:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal product = 1m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        product *= 1m;
+                    }
+                },
+                decimalMessage);
+        }
+
+        public static void Divide()
+        {
+            string intMessage = "Division of int:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    int result = 1000;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result /= 1;
+                    }
+                },
+                intMessage);
+
+            string longMessage = "Division of long:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    long result = 1000L;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result /= 1L;
+                    }
+                },
+                longMessage);
+
+            string floatMessage = "Division of float:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float result = 1000f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result /= 1f;
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Division of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double result = 1000.0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result /= 1.0;
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Division of decimal:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal result = 1000m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result /= 1m;
+                    }
+                },
+                decimalMessage);
+        }
+
+        public static void Main()
+        {
+            Add();
+            Console.WriteLine();
+            Subtract();
+            Console.WriteLine();
+            Increment();
+            Console.WriteLine();
+            Multiply();
+            Console.WriteLine();
+            Divide();
+        }
+    }
+}
---
+++ High Quality Code/HCQ-09-Code-Tuning-and-Optimization/MathOperationsPerfComparison/MathOperations.cs
@@ -0,0 +1,150 @@
+﻿namespace MathOperationsPerfComparison
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class MathOperations
+    {
+        public const int IterationsCount = 1000000;
+        public const int PaddingCount = 30;
+
+        public static void DisplayPerformanceTime(Action action, string message)
+        {
+            Console.Write(message);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed + System.Environment.NewLine);
+        }
+
+        private static void DisplaySquareRootPerformance()
+        {
+
+            string floatMessage = "Square root of float:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float result = 0f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = (float)Math.Sqrt(8.5);
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Square root of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double result = 0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = Math.Sqrt(8.5);
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Square root of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal result = 0m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = (decimal)Math.Sqrt(8.5);
+                    }
+                },
+                decimalMessage);
+        }
+
+        private static void DisplayNaturalLogarithmPerformance()
+        {
+            string floatMessage = "Natural logarithm of float:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float result = 0f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = (float)Math.Log(8.5, Math.E);
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Natural logarithm of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double result = 0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = Math.Log(8.5, Math.E);
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Natural logarithm of decimal:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal result = 0m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = (decimal)Math.Log(8.5, Math.E);
+                    }
+                },
+                decimalMessage);
+        }
+
+        private static void DisplaySinusPerformance()
+        {
+            string floatMessage = "Sinus of float:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    float result = 0f;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = (float)Math.Sin(8.5);
+                    }
+                },
+                floatMessage);
+
+            string doubleMessage = "Sinus of double:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    double result = 0;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = Math.Sin(8.5);
+                    }
+                },
+                doubleMessage);
+
+            string decimalMessage = "Sinus of decimal:".PadRight(PaddingCount);
+            DisplayPerformanceTime(
+                () =>
+                {
+                    decimal result = 0m;
+                    for (int i = 0; i < IterationsCount; i++)
+                    {
+                        result = (decimal)Math.Sin(8.5);
+                    }
+                },
+                decimalMessage);
+        }
+
+        static void Main(string[] args)
+        {
+            DisplaySquareRootPerformance();
+            Console.WriteLine();
+            DisplayNaturalLogarithmPerformance();
+            Console.WriteLine();
+            DisplaySinusPerformance();
+            Console.WriteLine("All operations take parameter of type DOUBLE and return result of type DOUBLE ==> casting is necessary for using these opperations with FLOAT and DECIMAL. That`s why the results from float and decimal operations must be ignored.");
+        }
+    }
+}
